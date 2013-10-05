810a9cba1033a4f81d25147563ced2b4fca057fc
nikhil275 <nikhil.sharma275@gmail.com>
2013-10-05 09:44:39 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    username : '' // USERNAME GOES HERE
+  , password : '' // PASSWORD GOES HERE
+  , botName: 'mnbot'
+  , adminIds : ["14022630"] //Array of users who have access to admin commands
+  , auth : "izutReno2TKBhyXcA6feiFuI30g=?_expires=STEzOTU4NTIyOTgKLg==&user_id=Uyc1MWNkMDc5ODg3N2I5MjYzNzMyYmE4YTknCnAxCi4=" //
+  , room : 'world-war-music'
+  , ga: {
+      account: '' // accountID for Google Analytics, if you want it.
+    }
+  , lastfm: {
+	key: '',
+	secret: '',
+	username: '',
+	password: ''
+  }
+  , disabledCommands: ['erm']
+  , welcomeNewDjs: false
+  , commandPrefix: '&'
+}
